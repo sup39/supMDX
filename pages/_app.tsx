d7efcc10736cceabae5918af85869d43df53d053
sup39 <dev@sup39.dev>
2022-11-05 02:42:47 +0900

    fix extendedHx href

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import '../styles/index.sass';
 const hx = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const;
 const extendedHx = Object.fromEntries(hx.map(H => [H,
   ({children, id, ...props}: React.ComponentProps<(typeof hx)[number]>) => <H id={id} {...props}>
-    {id && <a className="anchor" />}
+    {id && <a className="anchor" href={'#'+encodeURIComponent(id)} />}
     {children}
   </H>,
 ]));
