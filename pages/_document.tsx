9bb235133b7258ae37fa06bc8e2cd03f1b160967
sup39 <sup39@sup39.dev>
2022-11-05 01:39:51 +0900

    make css external

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import {Html, Head, Main, NextScript} from 'next/document';
+
+export default function Document() {
+  return <Html>
+    <Head>
+      <link rel="stylesheet" type="text/css" href="https://cdn.sup39.dev/css/index.css" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>;
+}
